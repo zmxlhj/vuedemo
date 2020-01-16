072a10e6b200c76a5fc5759e7731e99301bbdf3b
zmxlhj <2464844458@qq.com>
2020-01-16 17:07:50 +0800

    666

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <!--
  * @Author: your name
  * @Date: 2020-01-11 16:54:07
- * @LastEditTime : 2020-01-16 15:45:13
+ * @LastEditTime : 2020-01-16 16:53:57
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vuedemo\src\App.vue
  -->
 <template>
   <div id="app">
+    <router-view/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/user">User</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang='stylus' scoped>
+<style scoped>
+   #app{
+     position: relative;
+   }
+   #nav{
+     position: fixed;
+     left: 0;
+     bottom: 0;
+     width: 100%;
+     display: flex;
+     justify-content: space-around;
+     align-items: center;
+     height: 44px;
+   }
 </style>
